bed5939bb0e04bac2bf9d276578fb6f48f6744b0
U3Vzc3kgQW1vZ3Vz <99819877+U3Vzc3kgQW1vZ3Vz@users.noreply.github.com>
2022-03-29 12:07:42 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DictionaryManagement dict = new DictionaryManagement();
-        dict.insertFromFile();
-        DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
-        dictionaryCommandLine.dictionaryAdvanced();
-    }
-}
