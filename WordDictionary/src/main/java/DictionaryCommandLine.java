a9820cd2499965ef483a575b49100990b27200ea
U3Vzc3kgQW1vZ3Vz <99819877+U3Vzc3kgQW1vZ3Vz@users.noreply.github.com>
2022-04-03 18:25:14 +0700

    Delete WordDictionary/src/main directory

--- WordDictionary/src/main/java/DictionaryCommandLine.java
+++
@@ -1,8 +0,0 @@
-public class DictionaryCommandLine {
-    public void dictionaryBasic(){
-        DictionaryCommandline dict1=new DictionaryCommandline();
-        DictionaryManagement dict2=new DictionaryManagement();
-        dict2.insertFromCommandline();
-        dict1.showAllWords();
-    }
-}
--- WordDictionary/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        DictionaryManagement dict=new DictionaryManagement();
-        dict.insertFromFile();
-    }
-}
